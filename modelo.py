7b80394be09fd5d5e7a87b7642e682f328739f47
grupythonUFLA <pythonufla@gmail.com>
2012-06-26 14:25:27 -0300

    leves mudanças e modulo modelo

---
+++ modelo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import random 
+
+def extrai_conteudo(nome_arq):
+	lista = []
+	with open(nome_arq,'r') as arq:
+		for substantivo in arq:
+			lista.append(substantivo.strip())	
+	return lista
+	 
+lista_sub_fem = extrai_conteudo('sub_fem.txt')
+lista_sub_mas = extrai_conteudo('sub_mas.txt')
+lista_adj_mas = extrai_conteudo('adj_mas.txt')
+lista_adj_fem = extrai_conteudo('adj_fem.txt')
+
+class Senha:
+	
+	def	__init__(self):
+		self.valor = self.gera_senha()
+			
+
+	def gera_senha(self):
+		genero = random.choice(['m','f'])
+		if genero=='m':
+			return random.choice(lista_sub_mas)+\
+				random.choice(lista_adj_mas)+\
+				'{:03}'.format(random.choice(range(100)))
+		else:
+			return random.choice(lista_sub_fem)+\
+				random.choice(lista_adj_fem)+\
+				'{:03}'.format(random.choice(range(100)))
