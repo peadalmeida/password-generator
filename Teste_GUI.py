7dcee324f57c2e7e0b2416fa1dc6aef1d5b7008c
Pedro Augusto <pedroaugusto@pedroaugusto.(none)>
2012-06-27 18:30:40 -0300

    Código alterado conforme PEP8

--- Teste_GUI.py
+++ Teste_GUI.py
@@ -4,11 +4,11 @@ app = Tk()
 app.title("Gerador de Senhas")
 app.geometry('500x200+300+200')
 
-v = stringvar()
+v = StringVar()
 Label(app, textvariable=v, font=("Helvetica", 30)).pack()
 v.set("Senha gerada aqui")
 
-gera = button(app, text="Gerar!", width=10)
+gera = Button(app, text="Gerar!", width=10)
 gera.pack(side='left', padx=10, pady=10)
 
 sair = Button(app, text="Sair", width=10, command=app.quit)
